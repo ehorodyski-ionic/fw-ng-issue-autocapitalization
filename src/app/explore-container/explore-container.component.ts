e5675ccc1108dbda3656918962a747f362bb4131
Sugam Mankad <sugam.mankad@amtrak.com>
2020-07-08 15:40:41 -0400

    Ion Input Autocapitalize Words error

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -6,8 +6,6 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./explore-container.component.scss'],
 })
 export class ExploreContainerComponent implements OnInit {
-  @Input() name: string;
-
   constructor() { }
 
   ngOnInit() {}
